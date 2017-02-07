7460197e018eea0e84fa6c9ee3cd3c321b60b02b
demon <noparkinghere@foxmail.com>
2017-02-07 10:15:42 +0800

    update

--- up.sh
+++ up.sh
@@ -2,7 +2,7 @@
 
 git pull origin master
 hexo deploy
-echo "hexo build and run the shell for pushing your blog to github"
-git add .
-git commit -m "update blog"
-git push origin master
+#echo "hexo build and run the shell for pushing your blog to github"
+#git add .
+#git commit -m "update blog"
+#git push origin master
